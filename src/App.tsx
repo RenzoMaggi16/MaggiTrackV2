2bc4a24d147bc7b61663b68c5c3b9908f3499aba
RenzoMaggi16 <maggirenzo13@gmail.com>
2025-10-18 14:17:44 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import TradeDetail from "./pages/TradeDetail";
+import AnalisisIA from "./pages/AnalisisIA";
+import MisReglas from "./pages/MisReglas";
+import { ThemeProvider } from "./context/ThemeProvider";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <ThemeProvider defaultTheme="dark">
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/trades/:id" element={<TradeDetail />} />
+            <Route path="/analisis" element={<AnalisisIA />} />
+            <Route path="/reglas" element={<MisReglas />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </ThemeProvider>
+);
+
+export default App;
